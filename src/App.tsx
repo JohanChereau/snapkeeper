6fa49a114470d132110bffe4581312957dbcd763
JohanChereau <johanchereau2@gmail.com>
2024-07-08 21:05:41 +0200

    init: installation and config

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import { Button } from "./components/ui/button";
+
+const App = () => {
+  return (
+    <>
+      <h1 className="text-3xl font-bold underline text-red-500">
+        Snapkeeper starter
+      </h1>
+      <Button>Get started</Button>
+    </>
+  );
+};
+
+export default App;
